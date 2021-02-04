ad7e720d20c201b90c4b59c91926d179d1f33bc6
OlgaChaganova <68281687+OlgaChaganova@users.noreply.github.com>
2021-02-04 14:03:52 +0500

    Update RLS-app.py

--- RLS-app.py
+++ RLS-app.py
@@ -30,7 +30,7 @@ if uploaded_file is not None:
   st.subheader('Таблица с исходными данными')
   st.dataframe(np.array(y).reshape(1, len(y)))
   
-  st.write("График временного ряда")
+  st.write("График временного ряда", width=300, height=900)
   ax = plt.plot(list(range(len(y))), y.iloc[:,0])
   _ = plt.title("График временного ряда")
   st.pyplot()
